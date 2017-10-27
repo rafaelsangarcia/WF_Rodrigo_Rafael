056f599bad4dd3a194b453dc2454e941eada1326
rafaelsangarcia <32914130+rafaelsangarcia@users.noreply.github.com>
2017-10-27 12:58:55 -0500

    edit main.c

--- Window_Lifter_RR/src/main.c
+++ Window_Lifter_RR/src/main.c
@@ -12,10 +12,7 @@
 /*============================================================================*/
 /* DESCRIPTION :                                                              */
 /** \file
-    short description in one sentence end with dot.
-    detailed
-    multiline
-    description of the file
+    Rhis is the main.c file
 */
 /*============================================================================*/
 /* COPYRIGHT (C) CONTINENTAL AUTOMOTIVE 2014                                  */
